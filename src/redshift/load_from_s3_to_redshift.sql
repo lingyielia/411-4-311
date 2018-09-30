40c15da6266f240ee09d7cc0a229c4c203f8389d
lingyielia <31973683+lingyielia@users.noreply.github.com>
2018-09-30 13:58:02 -0400

    connect redshift with data in s3

---
+++ src/redshift/load_from_s3_to_redshift.sql
@@ -0,0 +1,22 @@
+-- create table
+create table events (
+  agency                 varchar(255)     not null,
+  closed_date            timestamp        not null,
+  complaint_type         varchar(255)     not null,
+  created_date           timestamp        not null,
+  latitude               double precision not null,
+  longitude              double precision not null,
+  open_data_channel_type varchar(255)     not null
+);
+
+-- check the table being successfully built
+select distinct(tablename) from PG_TABLE_DEF where schemaname = 'public';
+
+-- temperarily change the concurrency level to 2
+set wlm_query_slot_count to 2;
+
+-- copy data from s3
+copy events from 's3://nyc311forinsight/records_'
+credentials 'aws_access_key_id=<>;aws_secret_access_key=<>'
+csv
+timeformat 'YYYY-MM-DDTHH:MI:SS';
